0647e991e77f5f02734cfcce4f5b0f9887b63db7
SeaniceNabasirye <seanice877@gmail.com>
2025-07-24 17:07:37 +0300

    Add files via upload
    created a python folder then included these python files

---
+++ simple_mjpeg_server.py
@@ -0,0 +1,23 @@
+import cv2
+from flask import Flask, Response
+
+app = Flask(__name__)
+
+def generate():
+    cap = cv2.VideoCapture(0)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        _, jpeg = cv2.imencode('.jpg', frame)
+        frame_bytes = jpeg.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
